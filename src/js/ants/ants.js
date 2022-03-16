8007119b4d328d36ea7bc9582af695203ed17fc8
Cortuzz <oleg.cortuzz1@gmail.com>
2022-03-16 10:36:02 +0700

    Global ant algorithm change

--- src/js/ants/ants.js
+++ src/js/ants/ants.js
@@ -2,23 +2,28 @@ const BORDER = -2;
 
 
 class AntsSimulation {
-    constructor(field, width, height, greed, gregariousness, colony) {
+    constructor(field, width, height, greed, gregariousness, speed, pheromoneDecay, redPheromoneDecay, colony) {
         this.width = width;
         this.height = height;
         this.greed = greed;
         this.gregariousness = gregariousness;
+        this.speed = speed;
+        this.pheromoneDecay = pheromoneDecay;
+        this.redPheromoneDecay = redPheromoneDecay;
 
-        this.foodFound = false;
         this.field = field;
         this.pheromoneMap = [ ];
+        this.redPheromoneMap = [ ];
         this.pheromonePoints = [ ];
 
         this.colony = colony;
 
         for (let i = 0; i < this.height; i++) {
             this.pheromoneMap[i] = [ ];
+            this.redPheromoneMap[i] = [ ];
             for (let j = 0; j < this.width; j++) {
-                this.pheromoneMap[i][j] = 1;
+                this.pheromoneMap[i][j] = 0;
+                this.redPheromoneMap[i][j] = 0.5;
             }
         }
     }
@@ -28,30 +33,27 @@ class AntsSimulation {
     }
 
     getMoveProbability(x, y, ant) {
-        let pheromoneValue = this.pheromoneMap[y][x];
+        let redPheromoneValue = this.redPheromoneMap[y][x];
         let distance = this.getDistance(x, y, this.colony) + 1;
 
-        if (ant.found) {
-            return Math.pow(1 / distance, this.gregariousness);
-        } else if (this.foodFound) {
-            return Math.pow(pheromoneValue, this.greed) / distance;
-        } else {
-            let distancePwd = Math.pow(distance, this.greed);
-            if (distance > 100) {
-                return distancePwd + 0.05 * (2 * Math.random() - 1) / distancePwd;
-            }
-            return distancePwd + 0.1 * (2 * Math.random() - 1) / distancePwd;
-        }
+        return Math.pow(-2 * (redPheromoneValue - 1), this.gregariousness) / Math.pow(distance, this.greed);
     }
 
     sprayPheromones(x, y, value) {
-        this.pheromoneMap[y][x] = value;
+        this.pheromoneMap[y][x] += value;
         this.pheromonePoints[this.pheromonePoints.length] = { x: x, y: y };
     }
 
+    sprayRedPheromones(x, y, dif) {
+        if (this.redPheromoneMap[y][x] - dif > 0) {
+            this.redPheromoneMap[y][x] -= dif;
+        }
+    }
+
     getPossibleMoves(ant) {
-        let speed = 1;
-        let moves = [[speed, 0], [0, speed], [-speed, 0], [0, -speed]];
+        let moves = [[this.speed, 0], [0, this.speed], [-this.speed, 0], [0, -this.speed],
+            [this.speed, this.speed], [this.speed, -this.speed],
+            [-this.speed, this.speed], [-this.speed, -this.speed]];
         let possibleMoves = [ ];
         let count = 0;
 
@@ -59,86 +61,191 @@ class AntsSimulation {
             let moveX = ant.x + move[0];
             let moveY = ant.y + move[1];
 
-            if (ant.found && ant.path[0].x === moveX && ant.path[0].y === moveY) {
-                ant.path = [ ];
-                ant.found = false;
-                return [ {x: moveX, y: moveY} ]
-            }
-
-            if (!(moveX < 0 || moveY < 0 || moveX >= this.width || moveY >= this.height ||
-                this.field[moveY][moveX] === BORDER)) {
-                    possibleMoves[count] = {x: moveX, y: moveY};
+            if (this.checkBounds(moveX, moveY)) {
+                    possibleMoves[count] = { x: moveX, y: moveY };
                     count++;
             }
-            else if (!this.foodFound) {
-                ant.alive = false;
-            }
         });
+
         return shuffle(possibleMoves);
+    }
+
+    checkBounds(x, y) {
+        if (x < 0 || y < 0 || x >= this.width || y >= this.height || this.field[y][x] === BORDER) {
+            return false;
+        }
+        return true;
     }
 
     pheromoneTick() {
         this.pheromonePoints.forEach(point => {
             let pheromone = this.pheromoneMap[point.y][point.x];
-            if (pheromone > 1) {
-                pheromone--;
+            if (pheromone > 0) {
+                this.pheromoneMap[point.y][point.x] -= this.pheromoneDecay;
             }
         })
+
+        return;
+        for (let i = 0; i < this.height; i++) {
+            for (let j = 0; j < this.width; j++) {
+                this.redPheromoneMap[i][j] += this.redPheromoneDecay;
+            }
+        }
     }
 }
 
 
 class Colony {
-    constructor(x, y, size) {
+    constructor(x, y, size, antDirectionRandomBorder) {
         this.x = x;
         this.y = y;
+        this.simulation = null;
+        this.antDirRand = antDirectionRandomBorder;
 
         this.size = size;
         this.ants = [ ];
+    }
 
+    setAnts(simulation) {
+        this.simulation = simulation;
         for (let i = 0; i < this.size; i++) {
-            this.ants[i] = new Ant(this.x, this.y);
+            this.ants[i] = new Ant(this.x, this.y, this.simulation, this.antDirRand);
         }
     }
 
     addAnt() {
-        this.ants[this.ants.length] = new Ant(this.x, this.y);
+        this.ants[this.ants.length] = new Ant(this.x, this.y, this.simulation, this.antDirRand);
     }
 }
 
 
 class Ant {
-    constructor(x, y) {
-        this.alive = true;
+    constructor(x, y, simulation, randomBorder) {
+        this.simulation = simulation;
+        this.pheromones = 0.8;
+        this.pheromonesDecay = 0;
         this.found = false;
-        this.pheromones = 5000;
         this.x = x;
         this.y = y;
-        this.path = [ {x: this.x, y:this.y} ];
+
+        this.prevX = null;
+        this.prevY = null;
+        this.randomBorder = randomBorder;
+
+        this.difX = 1;
+        this.difY = 0;
     }
 
     move(x, y) {
+        let pheromones = 0.05;
+        this.difX = x - this.x;
+        this.difY = y - this.y;
+
+        this.prevX = this.x;
+        this.prevY = this.y;
+
         this.x = x;
         this.y = y;
-        if (!this.found) {
-            this.path[this.path.length] = {x: this.x, y: this.y};
+
+
+        if (this.found) {
+            this.pheromones -= this.pheromonesDecay;
+            this.simulation.sprayPheromones(this.x, this.y, this.pheromones);
+            pheromones *= -1;
         }
+
+        if (this.pheromones < 0) {
+            this.pheromones = 0.8;
+            this.found = false;
+        }
+
+        this.simulation.sprayRedPheromones(this.x, this.y, pheromones);
     }
 
     getLastMoves() {
-        return {current: {x: this.x, y: this.y}, previous: this.path[this.path.length - 2]};
+        return {current: {x: this.x, y: this.y}, previous: {x: this.prevX, y: this.prevY}};
     }
 
-    getPheromoneCount(pheromoneMap) {
-        let count = 1;
-        this.path.forEach(point => {
-            count += pheromoneMap[point.y][point.x];
-        })
+    tryGreenPheromone(moves) {
+        let moved = false;
+        let changeValue = Math.random();
 
-        return count;
+        moves.forEach(move => {
+            let greenPheromone = this.simulation.pheromoneMap[move.y][move.x];
+            if (greenPheromone > changeValue) {
+                moved = true;
+
+                this.difX = move.x - this.x;
+                this.difY = move.y - this.y;
+            }
+        });
+
+        if (!moved) {
+            return undefined;
+        }
+
+        let moveX = this.x + this.difX;
+        let moveY = this.y + this.difY;
+
+        return { x: moveX, y: moveY };
     }
 
-    getPathPheromones() {
+    getRandomDirection() {
+        let moves = shuffle(this.simulation.getPossibleMoves(this));
 
+        let changeValue = Math.random();
+
+        let greenPheromoneMove = this.tryGreenPheromone(moves);
+        if (greenPheromoneMove != null) {
+            return greenPheromoneMove;
+        }
+
+        if (changeValue < this.randomBorder) {
+            this.difX = moves[0].x - this.x;
+            this.difY = moves[0].y - this.y;
+        }
+
+        let moveX = this.x + this.difX;
+        let moveY = this.y + this.difY;
+
+        return { x: moveX, y: moveY };
+    }
+
+    getRandomDirectionByPheromones() {
+        let moves = shuffle(this.simulation.getPossibleMoves(this));
+        let moved = false;
+        let bestRedMove;
+        let bestRedPheromone = -100000000;
+        let changeValue = Math.random();
+
+        let greenPheromoneMove = this.tryGreenPheromone(moves);
+        if (greenPheromoneMove != null) {
+            return greenPheromoneMove;
+        }
+
+        moves.forEach(move => {
+            let redPheromone = this.simulation.redPheromoneMap[move.y][move.x];
+            if (redPheromone > changeValue) {
+                moved = true;
+
+                this.difX = move.x - this.x;
+                this.difY = move.y - this.y;
+            }
+
+            if (redPheromone > bestRedPheromone) {
+                bestRedMove = move;
+                bestRedPheromone = redPheromone;
+            }
+        });
+
+        if (!moved && bestRedMove != null) {
+            this.difX = bestRedMove.x - this.x;
+            this.difY = bestRedMove.y - this.y;
+        }
+
+        let moveX = this.x + this.difX;
+        let moveY = this.y + this.difY;
+
+        return { x: moveX, y: moveY };
     }
 }
--- src/js/ants/handler.js
+++ src/js/ants/handler.js
@@ -3,7 +3,42 @@ window.addEventListener("load", () => {
     window.startButton.addEventListener("click", startAnts);
 
     window.fieldButtons.addEventListener("click", changeMode);
+
+    window.locker.addEventListener("click", changeLock);
+
+    window.pathViewChecker.addEventListener("click", e => {renderFullPath = e.checked; })
+
+    window.changePopulation.addEventListener("input", e =>
+    { colonySize = +e.target.value; window.populationView.textContent = colonySize; });
+
+    window.changeGreed.addEventListener("input", e =>
+    { greed = +e.target.value; window.greedView.textContent = greed; });
+
+    window.changeGregariousness.addEventListener("input", e =>
+    { gregariousness = +e.target.value; window.gregariousnessView.textContent = gregariousness; });
+
+    window.changeAntSpeed.addEventListener("input", e =>
+    { speed = +e.target.value; window.speedView.textContent = speed; });
+
+    window.changePheromoneDecay.addEventListener("input", e =>
+    { pheromoneMultiplier = +e.target.value; window.pheromoneDecayView.textContent = pheromoneMultiplier; });
+
+    window.changeRedPheromoneDecay.addEventListener("input", e =>
+    { redPheromoneMultiplier = +e.target.value; window.redPheromoneDecayView.textContent = redPheromoneMultiplier; });
+
+    window.changeDecay.addEventListener("input", e =>
+    { decay = +e.target.value / 100; window.decayView.textContent = (100 * decay).toFixed(2) + '%'; });
 });
+
+let defaultLog = "Алгоритм не запущен";
+let defaultColor = "coral";
+
+let renderFullPath = true;
+let unlock = false;
+let colonySize = 1000;
+let decay = 0.75, greed = 0.4, gregariousness = 0.75, speed = 5;
+const pheromoneDecay = 0.0001, redPheromoneDecay = 0.000001;
+let pheromoneMultiplier = 1, redPheromoneMultiplier = 1;
 
 const EMPTY = -1, BORDER = -2, COLONY = -3;
 let colony;
@@ -22,11 +57,51 @@ function init() {
     viewStates = {'colony': "Установка колонии", 'food': "Установка еды",
         'border': "Установка преград", 'clearBorder': "Очистка преград"};
 
+    window.changePopulation.value = colonySize;
+    window.populationView.textContent = colonySize;
+
+    window.changeGreed.value = greed;
+    window.changeGregariousness.value = gregariousness;
+    window.changeAntSpeed.value = speed;
+    window.changePheromoneDecay.value = pheromoneMultiplier;
+    window.changeRedPheromoneDecay.value = redPheromoneMultiplier;
+    window.changeDecay.value = 100 * decay;
+
     ctx.fillStyle = "aliceblue";
     ctx.rect(0, 0, 1200, 600);
     ctx.fill();
     window.currentActionView.innerText = viewStates[currentState];
     fieldBuilder();
+}
+
+function changeLock(event) {
+    let dangerParams = document.querySelectorAll(".dangerParam");
+    let dangerParamsView = document.querySelectorAll(".dangerParamView");
+
+    unlock = this.checked;
+    if (unlock) {
+        window.lockerView.innerText = "Параметры разблокированы";
+        dangerParams.forEach(param => {
+            param.disabled = false;
+        })
+
+        window.greedView.textContent = greed;
+        window.gregariousnessView.textContent = gregariousness;
+        window.speedView.textContent = speed;
+        window.pheromoneDecayView.textContent = pheromoneMultiplier;
+        window.redPheromoneDecayView.textContent = redPheromoneMultiplier;
+        window.decayView.textContent = (100 * decay).toFixed(2) + '%';
+
+    } else {
+        window.lockerView.innerText = "Параметры заблокированы";
+        dangerParams.forEach(param => {
+            param.disabled = true;
+        })
+
+        dangerParamsView.forEach(paramView => {
+            paramView.textContent = "Заблокировано";
+        })
+    }
 }
 
 function fieldBuilder() {
@@ -38,7 +113,14 @@ function fieldBuilder() {
     }
 }
 
-function changeMode(event) {
+async function changeMode(event) {
+    if (running) {
+        changeLog("Отказано, алгоритм запущен", "B72626");
+        await sleep(3000);
+        changeLog("Алгоритм запущен", "lightgreen");
+        return;
+    }
+
     let action = event.target.dataset.mode;
 
     if (action == null) {
@@ -91,11 +173,14 @@ function convertCanvasToMatrix(rawData, w, h) {
 }
 
 function clearField() {
-
+    colonyPoint = undefined;
+    ctx.fillStyle = "aliceblue";
+    ctx.rect(0, 0, 1200, 600);
+    ctx.fill();
 }
 
 function drawAnt(x, y, color, w) {
-    if (checkFood(x, y)) {
+    if (checkFood(x, y) || field[y][x] === COLONY) {
         return;
     }
     let t = ctx.fillStyle;
@@ -107,74 +192,111 @@ function drawAnt(x, y, color, w) {
 function checkFood(x, y) {
     return field[y][x] > 0;
 }
-
+let path = [ ];
 async function antsAlg(simulation, colony) {
+
+
     for (let i = 0; i < colony.ants.length; i++) {
         let ant = colony.ants[i];
 
-        if (!ant.alive) {
-            colony.ants.splice(i, 1);
-            colony.addAnt();
-            continue;
-        }
-
-        let moves = simulation.getPossibleMoves(ant);
-
         if (ant.found) {
-            simulation.sprayPheromones(moves[0].x, moves[0].y, ant.pheromones);
-            ant.move(moves[0].x, moves[0].y);
-        }
-        let bestMove;
-        let bestProb = 0;
+            let moves = simulation.getPossibleMoves(ant);
+            let bestMove;
+            let bestValue = 0;
 
-        moves.forEach(move => {
-            let prob = simulation.getMoveProbability(move.x, move.y, ant);
-            if (prob > bestProb) {
-                bestProb = prob;
-                bestMove = move;
+            moves.forEach(move => {
+                let prob = simulation.getMoveProbability(move.x, move.y, ant);
+                if (bestValue < prob) {
+                    bestValue = prob;
+                    bestMove = move;
+                }
+
+            /*moves.forEach(move => {
+                let prob = simulation.getMoveProbability(move.x, move.y, ant);
+                if (Math.random() < prob) {
+                    ant.move(move.x, move.y, simulation);
+                    if (checkFood(move.x, move.y)) {
+                        ant.found = true;
+                    }
+                }*/
+
+
+            });
+            if (bestMove != null) {
+                ant.move(bestMove.x, bestMove.y);
+                if (simulation.getDistance(bestMove.x, bestMove.y, colony) === 0) {
+                    ant.found = false;
+                }
             }
+        } else {
+            let move = ant.getRandomDirectionByPheromones();
 
-            if (Math.random() < prob && simulation.foodFound) {
+            if (simulation.checkBounds(move.x, move.y)) {
                 ant.move(move.x, move.y);
-                if (checkFood(bestMove.x, bestMove.y)) {
+                if (checkFood(move.x, move.y)) {
+                    ant.pheromonesDecay = decay / simulation.getDistance(ant.x, ant.y, colony);
                     ant.found = true;
                 }
             }
-        });
-
-        if (!simulation.foodFound) {
-            if (checkFood(bestMove.x, bestMove.y)) {
-                simulation.foodFound = true;
-                ant.found = true;
-            }
-            ant.move(bestMove.x, bestMove.y);
         }
 
         let lastMoves = ant.getLastMoves();
 
-        if (ant.found) {
-            drawAnt(lastMoves.current.x, lastMoves.current.y, '#114411', 1, 1);
-            continue;
+        if (!ant.found) {
+            drawAnt(lastMoves.current.x, lastMoves.current.y, '#964B00', 3, 3);
+        } else {
+            path[path.length] = {ant: ant, x: lastMoves.current.x, y: lastMoves.current.y};
+            drawAnt(lastMoves.current.x, lastMoves.current.y, '#000000', 5, 5);
         }
-        drawAnt(lastMoves.current.x, lastMoves.current.y, '#964B00', 1, 1);
     }
 
-    simulation.pheromoneTick();
     await sleep(1);
 
     for (let i = 0; i < colony.ants.length; i++) {
         let ant = colony.ants[i];
         let lastMoves = ant.getLastMoves();
-        //drawAnt(lastMoves.current.x, lastMoves.current.y, '#F0F8FF', 1, 1);
+
+        drawAnt(lastMoves.current.x, lastMoves.current.y, '#F0F8FF', 3, 3);
     }
+
+    simulation.pheromoneTick();
+    if (renderFullPath) {
+        return;
+    }
+
+    while (path.length > 1000) {
+        let point = path.shift();
+        drawAnt(point.x, point.y, '#F0F8FF', 5, 5);
+    }
+}
+
+function changeLog(text, color) {
+    window.log.textContent = text;
+    window.log_block.style.borderColor = color;
 }
 
 async function startAnts() {
     let canvasData = ctx.getImageData(0, 0, 1200, 600);
     field = convertCanvasToMatrix(canvasData.data, canvasData.width, canvasData.height);
 
-    let colony = new Colony(+colonyPoint.x, +colonyPoint.y,1000);
-    let simulation = new AntsSimulation(field, 1200, 600, 0.1, 5, colony);
+    if (colonyPoint == null) {
+        changeLog("Отсутствует колония", "B72626");
+        await sleep(3000);
+
+        if (window.log.textContent !== "Алгоритм запущен") {
+            changeLog(defaultLog, defaultColor);
+        }
+        return;
+    }
+
+    running = true;
+    changeLog("Алгоритм запущен", "lightgreen");
+
+    let colony = new Colony(+colonyPoint.x, +colonyPoint.y, colonySize,0.01);
+    let simulation = new AntsSimulation(field, 1200, 600, greed, gregariousness, speed,
+        pheromoneMultiplier * pheromoneDecay, redPheromoneMultiplier * redPheromoneDecay, colony);
+
+    colony.setAnts(simulation);
 
     colony = simulation.colony;
     let epochs = 1000000;
