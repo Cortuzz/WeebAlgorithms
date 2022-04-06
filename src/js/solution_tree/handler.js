011c4e80f5ca188cf0960dc3090695295f147d87
alyoneek <alyonta03@mail.ru>
2022-04-06 22:38:57 +0700

    Solution tree

--- src/js/solution_tree/handler.js
+++ src/js/solution_tree/handler.js
@@ -1,5 +1,8 @@
-window.startButton.addEventListener("click", generateTree);
+window.startButton.addEventListener("click", printTree);
 let csvText = document.getElementById("csv");
+
+let headline;
+let tree;
 
 function csvNormalizer(text) {
     let data = [ ];
@@ -29,9 +32,53 @@ function csvNormalizer(text) {
         data.push(currentData);
     }
 
+    headline = data[0];
+    data.splice(0, 1);
+
+    if (isCorrect(data)) {
+        return toFloat(data);
+    }
+    else {
+        console.log("error");
+    }
+}
+
+function isCorrect(data) {
+    const columnsNumber = headline.length;
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].length !== columnsNumber) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function toFloat(data) {
+    for (let i = 0; i < headline.length; i++) {
+        let allNumbers = true;
+
+        for (let j = 0; j < data.length; j++) {
+            if (Number.isNaN(parseFloat(data[j][i]))) {
+                allNumbers = false;
+                break;
+            }
+        }
+
+        if (allNumbers) {
+            for (let j = 0; j < data.length; j++) {
+                data[j][i] = parseFloat(data[j][i]);
+            }
+        }
+    }
+
     return data;
 }
 
-function generateTree() {
+function printTree() {
     let trainingData = csvNormalizer(csvText.value);
+    let tree = new Tree(trainingData);
+    tree.createTree(0, trainingData);
+    console.log(tree);
 }
---
+++ src/js/solution_tree/tree.js
@@ -0,0 +1,165 @@
+class Node {
+    constructor(type, level, value, columnOfValue, name, impurity) {
+        this.type = type;
+        this.level = level;
+        this.value = value;
+        this.columnOfValue = columnOfValue;
+        this.name = name;
+        this.impurirty = impurity
+        this.trueBranch = null;
+        this.falseBranch = null;
+    }
+}
+
+class Tree {
+    constructor(data) {
+        this.trainingData = data;
+        this.root = null;
+    }
+
+    getAlignmentNumber(data, column, value) {
+        let count = 0;
+        for (let i = 0; i < data.length; i++) {
+            if (data[i][column] === value) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    getGiniImpurityForLeaf(data) {
+        let impurity = 1;
+        let uniqueParams = [];
+        let lastColumn = headline.length - 1;
+
+        for (let i = 0; i < data.length; i++) {
+            if (!uniqueParams.includes(data[i][lastColumn])) {
+                let probability = this.getAlignmentNumber(data, lastColumn, data[i][lastColumn]) / data.length;
+                impurity -= Math.pow(probability, 2);
+                uniqueParams.push(data[i][lastColumn]);
+            }
+        }
+
+        return impurity;
+    }
+
+    getTotalGiniImpurity(trueBranch, falseBranch) {
+        let probabilityTrue = trueBranch.length / (trueBranch.length + falseBranch.length);
+        let probabilityFalse = falseBranch.length / (trueBranch.length + falseBranch.length);
+        console.log(trueBranch, falseBranch)
+        console.log(this.getGiniImpurityForLeaf(trueBranch), this.getGiniImpurityForLeaf(falseBranch))
+        return probabilityTrue * this.getGiniImpurityForLeaf(trueBranch) + probabilityFalse * this.getGiniImpurityForLeaf(falseBranch);
+    }
+
+    createNameForNode(columnName, value) {
+        let name = columnName;
+
+        if (typeof value !== "string") {
+            name += ` >= ${value}`;
+        }
+        else {
+            name += ` = ${value}`;
+        }
+
+        name += " ?";
+        return name;
+    }
+
+    divideBranches(data, column, value) {
+        let trueBranch = [];
+        let falseBranch = [];
+
+        if (typeof value === "string") {
+            for (let i = 0; i < data.length; i++) {
+                if (data[i][column] === value) {
+                    trueBranch.push(data[i]);
+                }
+                else {
+                    falseBranch.push(data[i]);
+                }
+            }
+        }
+        else {
+            for (let i = 0; i < data.length; i++) {
+                if (data[i][column] >= value) {
+                    trueBranch.push(data[i]);
+                }
+                else {
+                    falseBranch.push(data[i]);
+                }
+            }
+        }
+
+        return [trueBranch, falseBranch];
+    }
+
+    //!!!!!!!!!
+    getBestSplit(data) {
+        let uniqueParams = [];
+        let bestSplitNode = new Node("", -1, "", -1, "", Infinity);
+
+        for (let i = 0; i < headline.length - 1; i++) {
+            for (let j = 0; j < data.length - 1; j++) {
+                if (!uniqueParams.includes(data[j][i])) {
+                    let nameForNode = this.createNameForNode(headline[i], data[j][i]);
+                    console.log(nameForNode);
+                    let [trueBranch, falseBranch] = this.divideBranches(data, i, data[j][i]);
+
+                    if (trueBranch.length === 0 || falseBranch.length === 0) {
+                        continue;
+                    }
+
+                    let currentImpurity = this.getTotalGiniImpurity(trueBranch, falseBranch);
+                    console.log(currentImpurity);
+
+                    if (currentImpurity <= bestSplitNode.impurirty) {
+                        bestSplitNode = new Node("", -1, data[j][i], i, nameForNode, currentImpurity);
+                    }
+
+                    uniqueParams.push(data[j][i]);
+                }
+            }
+        }
+
+        return bestSplitNode;
+    }
+
+    createLeaf(level, data) {
+        let uniqueParams = [];
+        let names = [];
+        let lastColumn = headline.length - 1;
+
+        for (let i = 0; i < data.length; i++) {
+            if (!uniqueParams.includes(data[i][lastColumn])) {
+                let probability = this.getAlignmentNumber(data, lastColumn, data[i][lastColumn]) / data.length;
+                names.push(`${data[i][lastColumn]}: ${probability * 100}`);
+                uniqueParams.push(data[i][lastColumn]);
+            }
+        }
+
+        return new Node("leaf", level, uniqueParams, lastColumn, names, -1);
+    }
+
+    createDecisionNode(level, node, trueBranch, falseBranch) {
+        node.type = "decision";
+        node.level = level;
+        node.trueBranch = trueBranch;
+        node.falseBranch = falseBranch;
+
+        return node;
+    }
+
+    createTree(level, data) {
+        let bestSplitNode = this.getBestSplit(data);
+        if (bestSplitNode.impurirty === Infinity) {
+            return this.createLeaf(level, data);
+        }
+
+        let [dataForTrueBranch, dataForFalseBranch] = this.divideBranches(data, bestSplitNode.columnOfValue, bestSplitNode.value);
+        level++;
+        let trueBranch = this.createTree(level, dataForTrueBranch);
+        let falseBranch = this.createTree(level, dataForFalseBranch);
+        this.root = this.createDecisionNode(level, bestSplitNode, trueBranch, falseBranch);
+        return this.root;
+    }
+}
